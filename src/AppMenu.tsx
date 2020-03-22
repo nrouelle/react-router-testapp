5d745463cb54e386efd4e96be241e3992cc8e407
Nadege Rouelle <nadege@rouelle.net>
2020-03-22 12:00:37 +0100

    adding routing and a protected route

---
+++ src/AppMenu.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import {BrowserRouter as Router, Link, Switch, Route, Redirect, RouteComponentProps} from 'react-router-dom';
+import Register from './Register';
+import Dashboard from './Dashboard';
+import Home from './Home';
+import Login from './Login';
+
+class AppMenu extends React.Component {
+    render() {
+        return (
+            <Router>
+                <ul>
+                  <li><Link to="/">Accueil</Link></li>
+                  <li><Link to="/register">Register</Link></li>
+                  
+                  <li><Link to="/Nadege">Nadege</Link></li>
+              </ul>
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/register">
+                  <Register />
+                </Route>
+                <Route path="/login">
+                  <Login />
+                </Route>
+                <NadComp>
+                  <Dashboard />
+                </NadComp>
+              </Switch>
+            </Router>
+        
+        );
+    }
+}
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate() {
+    fakeAuth.isAuthenticated = true;
+  }
+};
+
+class AdminLink extends React.Component<{}> {
+  render() {
+    return (
+      <li><Link to="/dashboard">Dashboard</Link></li>
+    )
+  }
+}
+
+class NadComp extends React.Component<{}> {
+   render() {
+    return (
+      fakeAuth.isAuthenticated ? (<AdminLink />) : 
+      (
+        <Redirect
+          to={{
+            pathname: "/login"
+          }}
+        />
+      )
+    );
+  }
+}
+
+export default AppMenu;
---
+++ src/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Login extends React.Component {
+    render() {
+        return (<h1>Page de login</h1>);
+    }
+}
+
+export default Login;
