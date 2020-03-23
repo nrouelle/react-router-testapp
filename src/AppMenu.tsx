c2001a1d152f87f971a276d89872e4157b826cd0
Nadege Rouelle <nadege@rouelle.net>
2020-03-23 20:13:47 +0100

    cleanup

--- src/AppMenu.tsx
+++ src/AppMenu.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {BrowserRouter as Router, Link, Switch, Route, Redirect, RouteComponentProps} from 'react-router-dom';
+import {BrowserRouter as Router, Link, Switch, Route, Redirect} from 'react-router-dom';
 import Register from './Register';
 import Dashboard from './Dashboard';
 import Home from './Home';
